a4c0f11fdae414770e139833d2df020db5de02c1
parkjiyonaffair@gmail.com <parkjiyonaffair@gmail.com>
2025-02-23 21:35:11 +0900

    main update

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from fastapi.staticfiles import StaticFiles
 from typing import Optional
 import shutil
 import os
-import uuid
 
 from fin2 import process_images
 from colclass import ColorClassifierApp
@@ -23,7 +22,7 @@ async def root():
     return HTMLResponse(content="<h1>Hello World</h1><p>Welcome to the Image Analysis API</p>")
 
 @app.post("/upload-image/")
-async def upload_image(file: Optional[UploadFile] = None):
+async def upload_image(file: UploadFile = File(...)):
 
     if file is None:
         raise HTTPException(status_code=400, detail="No file uploaded")
@@ -31,13 +30,10 @@ async def upload_image(file: Optional[UploadFile] = None):
     # 확장자 검사
     file_ext = os.path.splitext(file.filename)[1].lower()
     if file_ext not in ALLOWED_EXTENSIONS:
-        raise HTTPException(status_code=500, detail=f"Invalid file type: {file_ext}. Only PNG, JPG, JPEG are allowed.")
+        raise HTTPException(status_code=430, detail=f"Invalid file type: {file_ext}. Only PNG, JPG, JPEG are allowed.")
     
     # 1. 이미지 업로드
-
-    # 고유한 파일명 생성 (UUID 사용)
-    unique_filename = f"{uuid.uuid4().hex}{file_ext}"
-    file_path = os.path.join(UPLOAD_DIRECTORY, unique_filename)
+    file_path = os.path.join(UPLOAD_DIRECTORY, file.filename)
     
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
